ff77663ea9cfb092a9ef4fde0ee46024b02bab21
Tomaž Hribernik <th6727@student.uni-lj.si>
2022-01-03 08:03:24 +0100

    Replaced Makefile with make.sh
    This was done due to easier build scripting

---
+++ make.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+
+case "$1" in
+clean)
+	make -C xfe clean || true
+	quilt pop -a || true
+	rm -rf linux || true
+	rm -rf linux.orig || true
+	rm *.deb || true
+    ;;
+linux)
+	make -C linux clean
+	make -C linux olddefconfig
+    make -C linux -j 4 deb-pkg >build.log 2>&1
+    ;;
+xfe)
+    make -C xfe
+esac
