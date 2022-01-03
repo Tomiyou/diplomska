ae578cc1c5288cb06e462f89fb79fa0620c837bf
Tomaž Hribernik <th6727@student.uni-lj.si>
2022-01-03 08:44:01 +0100

    Fixed make.sh quilt operations

--- make.sh
+++ make.sh
@@ -3,12 +3,15 @@
 case "$1" in
 clean)
 	make -C xfe clean || true
-	quilt pop -a || true
+	quilt pop -a -f || true
 	rm -rf linux || true
 	rm -rf linux.orig || true
 	rm *.deb || true
+    rm *.gz || true
+    rm *.dsc || true
     ;;
 linux)
+    quilt push -a
 	make -C linux clean
 	make -C linux olddefconfig
     make -C linux -j 4 deb-pkg >build.log 2>&1
